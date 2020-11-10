3d109e6d3ea1a49600e239fd38a9e171b25b95b2
hangyeol0531 <jhan0531@naver.com>
2020-11-10 10:49:18 +0900

    ig_upload

--- FSMin/routes/index.js
+++ FSMin/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var multer = require('multer')
 var router = express.Router();
 
 /* GET home page. */
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/userimage/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf()+file.originalname);
+    }
+  }),
+});
+
 router.get('/', function(req, res, next) {
+  console.log('/ 접속')
   res.render('index', { title: 'Express' });
 });
+
+router.post('/save_Image', upload.single('user_images', (req,res) =>{
+  console.log('save Image 접속')
+}))
 
 module.exports = router;
