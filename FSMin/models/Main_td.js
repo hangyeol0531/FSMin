963c72d7fe5c561f013b58f16f87713f409b4f04
hangyeol0531 <jhan0531@naver.com>
2020-11-11 17:08:41 +0900

    table_model

---
+++ FSMin/models/Main_td.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+
+module.exports = ((sequelize,DataTypes)=>{
+    return sequelize.define('Main_td',{
+        idx:{
+            type: Sequelize.INTEGER,
+            primaryKey : true,
+            allowNull: true,
+            autoIncrement : true
+        },
+        tb_num:{
+            type: Sequelize.INTEGER,
+            allowNull: true,
+        },
+        src:{
+            type: Sequelize.STRING(100),
+            allowNull: true,
+        },
+    },{
+        timestamps:true,
+        paranoid : true, // 삭제일 (복구용)
+    })
+})
---
+++ FSMin/models/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+var main = require('./Main_td')(sequelize,Sequelize);
+var sub1 = require('./sub_td1')(sequelize,Sequelize);
+var sub2 = require('./sub_td2')(sequelize,Sequelize);
+var sub3 = require('./sub_td3')(sequelize,Sequelize);
+main.sync()
+sub1.sync()
+sub2.sync()
+sub3.sync()
+
+module.exports = db;
