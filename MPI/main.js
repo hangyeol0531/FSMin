046971aaa6ff513fc5573522de3199b540e67922
eungeon <ddolbangpark@naver.com>
2020-11-12 07:41:46 +0000

    [Modified] server conn error handling

--- MPI/main.js
+++ MPI/main.js
@@ -1,24 +1,36 @@
 const net = require('net');
 const file = require('./file');
+//const dns = require('dns');
 
 const hosts = require('../config.json').hosts;
 
 const getDiskSize = (nodeList, callback) =>{
     let arr = [];
+    let checked = 0;
     nodeList.forEach((host,idx) => {
-        const conn = net.createConnection(host.port, host.ip, ()=>{
+        const conn = net.connect(host.port, host.ip, ()=>{
+            conn.on('lookup',(err)=>{
+                console.log('asdfasfae')
+                console.log(err);
+            })
             conn.write('[GET] SIZE');
             conn.on('data', size =>{
-                arr.push(parseInt(size.toString()));    
+                arr[idx] = (parseInt(size.toString()));    
                 conn.end('OK');
-                console.log(idx)
-                if(idx+1 == nodeList.length) callback(arr);
+
+                if(++checked == nodeList.length) callback(arr);
             });
+        });
+        conn.on('error',(error)=>{
+            console.log(`Server Error (${host.ip}:${host.port})`);
+            arr[idx] = 'X';    
+            if(++checked == nodeList.length) callback(arr);
         });
     });
 };
 
+const ar = [{"ip":"localhost","port":5000}];
 
-getDiskSize([{"ip":"localhost","port":5000}],(data)=>{
+getDiskSize(hosts,(data)=>{
     console.log(data);
 });
