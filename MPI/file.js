1af1cb65189da6da4219acc08834152860bc2c92
eungeon <ddolbangpark@naver.com>
2020-11-17 10:20:58 +0000

    [Added] node server file

---
+++ MPI/file.js
@@ -0,0 +1,194 @@
+const fs = require('fs');
+const { exec } = require("child_process");
+const path = require('path');
+const net = require('net');
+
+module.exports.header = `FILE`;
+module.exports.delimiter = `@`;
+module.exports.tail = `END`;
+
+const bufHeader = Buffer.from(this.header);
+const bufDelimiter = Buffer.from(this.delimiter);
+const dir = '../disk'
+
+module.exports.recvFile = (hosts, filename, cb)=>{
+    let 
+};
+
+module.exports.delFile = (hosts, filename, cb)=>{
+    let checked = 0;
+    let arr = [];
+    hosts.forEach((host, idx)=>{
+        const conn = net.createConnection(host.port, host.ip, ()=>{
+            conn.write(`[DEL]${filename}`);
+
+            conn.on('data', data =>{
+                
+                if(data.toString() != 'OK'){
+                    arr[idx] = 'ERR';
+                }
+                if(++checked >= hosts.length){
+                    if(arr.length == 0 ) cb(undefined);
+                    else cb(arr);
+                    
+                }
+            });
+        });
+        conn.on('error',e =>{
+            arr[idx] = 'OFF';
+
+            if(++checked >= hosts.length){
+                cb(arr);
+            }
+        });
+        
+    });
+};
+
+module.exports.sendStreamSlave = (stream, conn, fileName, cb) => {
+    const bufLenHead = Buffer.from('SIZE');
+    const bufFileName = Buffer.from(fileName);
+    const nameLen = ('00' + bufFileName.length.toString(16)).slice(-2);
+    const bufNameLen = Buffer.from(nameLen);
+    console.log(`${fileName} -> ${fileName.length}`);
+
+    const namePack = Buffer.concat([bufLenHead,bufNameLen,bufFileName,bufDelimiter]);
+    conn.write(namePack);
+
+    //const rs = fs.createReadStream(filePath, {highWaterMark: blockSize});
+    let packages = 0;
+    let totalBytes = 0;
+
+    stream.on('data', chunk => {
+        packages++;
+
+        const packSize = ('00000' + chunk.length.toString(16)).slice(-5);
+        const bufFileSize = Buffer.from(packSize);
+        
+        console.log(`size: ${chunk.length} / hex: ${packSize.toString()}`);
+
+        const pack = Buffer.concat([bufHeader,bufFileSize,chunk,bufDelimiter]);
+        totalBytes += pack.length;
+        conn.write(pack);
+        console.log(pack);
+    });
+
+    stream.on('close', ()=>{
+        conn.end();
+        console.log("total packages", packages);
+        console.log("total bytes sent", totalBytes);
+        cb(packages,totalBytes);
+    });
+};
+
+module.exports.recvFileMaster = (conn, dir) =>{
+    let packets = 0;
+    let buffer = Buffer.alloc(0);
+
+    conn.on('data', (chunk) => {
+        console.log(chunk);
+        if(chunk != 'END'){
+            packets++;
+            
+            buffer = Buffer.concat([buffer, chunk]);
+        }
+    });
+
+    conn.on('close',()=>{
+        console.log("total packages", packets);
+        let fileName;
+
+        const head = buffer.subarray(0, 4);
+        console.log(head);
+        if(head.toString() == "SIZE"){
+            const nameLen = parseInt(buffer.slice(4, 6), 16);    
+            fileName = buffer.slice(6, nameLen + 6).toString();
+            console.log(fileName.toString());
+            buffer = buffer.slice(nameLen + 7, buffer.length);
+        }
+        
+        const writeStream = fs.createWriteStream(path.join(dir, fileName));
+        console.log("buffer size", buffer.length);
+        
+        while(buffer.length){
+            const head = buffer.slice(0, 4);
+            if(head.toString() != "FILE"){
+                console.log("Error Occurred.");
+                console.log(`header: ${head.toString()}`);
+                //process.exit(1);
+            }
+
+            const size = parseInt(buffer.slice(4, 9), 16);
+            console.log(`size : ${size}`);
+
+            const content = buffer.slice(9, size + 9);
+            const delimiter = buffer.slice(size + 9, size + 10);
+            
+            if(delimiter != "@"){
+                console.log(`Wrong delimiter. -> ${delimiter.toString()}`);
+            }
+
+            writeStream.write(content);
+            buffer = buffer.slice(size + 10, buffer.length);
+        }
+        console.log(`Download Successed : ${fileName}`)
+    })
+};
+
+module.exports.sendFileSlave = (conn, filePath, blockSize) => {
+    const bufLenHead = Buffer.from('SIZE');
+
+    const fileName = path.parse(filePath).base;
+    const bufFileName = Buffer.from(fileName);
+
+    const nameLen = ('00' + fileName.length.toString(16)).slice(-2);
+    const bufNameLen = Buffer.from(nameLen);
+
+    console.log(`${fileName} -> ${fileName.length}`);
+
+    const namePack = Buffer.concat([bufLenHead,bufNameLen,bufFileName,bufDelimiter]);
+
+    conn.write(namePack);
+
+    const rs = fs.createReadStream(filePath, {highWaterMark: blockSize});
+    let packages = 0;
+    let totalBytes = 0;
+
+    rs.on('data', chunk => {
+        packages++;
+
+        const packSize = ('0000' + chunk.length.toString(16)).slice(-4);
+        const bufFileSize = Buffer.from(packSize);
+        
+        console.log(`size: ${chunk.length} / hex: ${packSize.toString()}`);
+
+        const pack = Buffer.concat([bufHeader,bufFileSize,chunk,bufDelimiter]);
+        totalBytes += pack.length;
+        conn.write(pack);
+        console.log(pack);
+    });
+
+    rs.on('close', ()=>{
+        conn.end();
+        console.log("total packages", packages);
+        console.log("total bytes sent", totalBytes);
+    });
+};
+
+
+module.exports.getDirInfo = (path, callback) => {
+    exec(`du -sD ${path}`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        const size = stdout.toString().split('\t')[0];
+        
+        callback(size);
+        //return (stdout.toString().split('\t')[0]).toString;
+    });  
+};
---
+++ MPI/owfsStroageEngine.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const net = require('net');
+
+const sendFile = require('./file');
+
+function owfsStorage(opts){
+    this.getDestination = (opts.destination);
+    this.getFileName = (opts.filename);
+
+    
+}
+
+owfsStorage.prototype._handleFile = function _handleFile(req, file, cb){
+    var that = this;
+    
+    that.getDestination(req, file, function(err, dest){
+        if (err) return cb(err);
+        that.getFileName(req, file, function(err, filename){
+            let checked = 0;
+            let arr = [];
+            
+            dest.forEach((host, idx)=>{
+                if(err) return cb(err);
+                const conn = net.createConnection(host.port, host.ip, ()=>{
+                    conn.write('[POST]');
+                });
+                conn.on('data', data =>{
+                    if(data == 'OK'){
+                        console.log('sending');
+                        console.log(file)
+                        sendFile.sendStreamSlave(file.stream, conn, filename, (packcnt,packsize)=>{
+                            arr[idx] = 'OK';
+                            if(++checked == dest.length){
+                                cb(null, {
+                                    path: dest,
+                                    size: packsize,
+                                    filename: filename
+                                });
+                            }
+                        });
+                    }
+                })
+                conn.on('error', error=>{
+                    arr[idx] = 'X';
+                    console.log(`Server Error (${host.ip}:${host.port})`);
+                    if(++checked == dest.length){
+                        cb(host, {
+                            path: dest
+                        });
+                    }
+                });
+
+            });
+            
+            
+        });
+        
+        
+    });
+}
+
+owfsStorage.prototype._removeFile = function _removeFile(req, file, cb){
+    var that = this;
+    const filename = file.name;
+    that.getDestination(err, file, function(err, dest){
+        if(err) return cb(err);
+        const conn = net.createConnection(dest.port, dst.ip, ()=>{
+            conn.write(`[DEL]${filename}`);
+        });
+        conn.on('data', (data)=>{
+            if(data.toString() == 'OK'){
+                cb(undefined);
+                delete file.destination;
+                delete file.filename;
+                delete file.path;
+            }
+            else cb('err');
+        }); 
+        conn.on('error',(err)=>{
+            console.log(`Server Error (${host.ip}:${host.port})`);
+            cb(err);
+        });
+    });
+}
+
+module.exports = function(opts){
+    return new owfsStorage(opts);
+}
